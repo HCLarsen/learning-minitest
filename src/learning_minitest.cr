d0f95e9fde01656db54d4704a023edd62f9e8526
Chris Larsen <clarsenipod@gmail.com>
2018-01-27 02:49:25 -0500

    Initial Commit

---
+++ src/learning_minitest.cr
@@ -0,0 +1,6 @@
+require "./learning_minitest/*"
+
+# TODO: Write documentation for `LearningMinitest`
+module LearningMinitest
+  # TODO: Put your code here
+end
---
+++ src/learning_minitest/version.cr
@@ -0,0 +1,3 @@
+module LearningMinitest
+  VERSION = "0.1.0"
+end
